cad72911566310dab6b5def88b19999f10e433bf
Sal Ceja <salceja45@gmail.com>
2023-07-26 12:46:07 -0700

    before submitting

--- Assets/script.js
+++ Assets/script.js
@@ -29,25 +29,40 @@ var questions = [
     ],
     answer: "C - A collection of properties with key value pairs.",
   },
+  {
+    question:
+      "How much would would a woodchuck chuck if a wood chuck could chuck wood?",
+    choices: [
+      "A - 90 punds",
+      "B - 150 cubic yards",
+      "C - 60 yards",
+      "D - 361 cubic centimeters/ day",
+    ],
+    answer: "D - 361 cubic centimeters/ day",
+  },
 ];
 var questionIndex = 0;
 var interval;
 var timeLimit = 60;
-var score = 0;  // inital score.
+var score = 0; // inital score.
 
-//Event Handlers: 
+//Event Handlers:
 document.getElementById("startBtn").addEventListener("click", startQuiz);
-document.getElementById("quizScreen").addEventListener("click", multipleChoiceBtn);
+document
+  .getElementById("quizScreen")
+  .addEventListener("click", multipleChoiceBtn);
 document.getElementById("submit").addEventListener("click", submitScore);
-document.getElementById("goBack").addEventListener("click", startQuiz); 
-document.getElementById("clearScores").addEventListener("click", clearHighScores);// clearHighScores vs clearHighScores() when adding parenthesis, it forces click event to run right away.
-
+document.getElementById("goBack").addEventListener("click", goBack);
+document
+  .getElementById("clearScores")
+  .addEventListener("click", clearHighScores);
+// clearHighScores vs clearHighScores() when adding parenthesis, it forces click event to run right away.
 
 // functions:
-
 function startQuiz() {
   document.getElementById("startScreen").style.display = "none";
   document.getElementById("quizScreen").style.display = "block"; // block is the default.
+  document.getElementById("scoreboardScreen").style.display = "none";
   timerCountdown();
   renderNextQuestion();
 }
@@ -63,20 +78,20 @@ function timerCountdown() {
 
 function renderNextQuestion() {
   document.getElementById("answer").innerHTML = "";
+  document.getElementById("answer").setAttribute("style", "none"); // get rid of the border-top line.
 
-  if(questionIndex < 0 || questionIndex >= questions.length) {
+  if (questionIndex < 0 || questionIndex >= questions.length) {
     return;
   }
-
   var currentQuestion = questions[questionIndex];
-  
-  document.getElementById("question").innerText = currentQuestion.question; // sets the new question in the html
-  for (var i = 1; i < 5; i++) {
-    // populates buttons with letter choices.
-    document.getElementById("btn" + i).innerText =
-      currentQuestion.choices[i - 1];
-  }
 
+  // sets the new question in the html
+  document.getElementById("question").innerText = currentQuestion.question;
+  for (var i = 1; i < 5; i++) {
+    // populates buttons with choices array values, in questions array.
+    document.getElementById("btn" + i).innerText =
+      currentQuestion.choices[i - 1]; // [i - 1] bc btns id's start at 1. ex, btn1, btn2...
+  }
 }
 
 function multipleChoiceBtn(event) {
@@ -84,49 +99,64 @@ function multipleChoiceBtn(event) {
   var userChoice = event.target.innerText; // grabs the innertext from button
 
   if (event.target.nodeName === "BUTTON") {
-  
-    if (userChoice === correctChoice) { // correct choice selected.
+    if (userChoice === correctChoice) {
+      // correct choice selected.
       score++;
-      document.getElementById("answer").innerText = "Correct Choice!"; 
-      document.getElementById("answer").setAttribute("style", "color: green; font-style: italic;");    
-      
-    } else { // if (userChoice !== correctChoice) wrong choice selected.
-      timeLimit -= 10; // penalty
-      document.getElementById("answer").innerText ="Wrong Choice!";
-      document.getElementById("answer").setAttribute("style", "color: red; font-style: italic;");
+      document.getElementById("answer").innerText = "Correct Choice!";
+      document
+        .getElementById("answer")
+        .setAttribute(
+          "style",
+          "color: green; font-style: italic; border-top: 1px solid grey;"
+        );
+    } else {
+      //  wrong choice selected.
+      timeLimit -= 10; // time penalty
+      document.getElementById("answer").innerText = "Wrong Choice!";
+      document
+        .getElementById("answer")
+        .setAttribute(
+          "style",
+          "color: red; font-style: italic; border-top: 1px solid grey;"
+        );
+    }
+    questionIndex++;
 
-    }
-    questionIndex++; // updates the index + 1 
-    
-    if (questionIndex >= questions.length) { // when 3 >= 3
+    if (questionIndex >= questions.length) {
+      // ex, when 4 >= 4
+      setTimeout(function () {
+        document.getElementById("quizScreen").style.display = "none"; // hides quiz screen
+        document.getElementById("endScreen").style.display = "block"; // shows endScreen
+
+        document.getElementById(
+          "finalScore"
+        ).innerText = `Your final score is: ${score}`;
+        console.log("Here are the final score: " + score);
+      }, 1000);
       clearInterval(interval); // stops the timer.
-      
-      document.getElementById("quizScreen").style.display = "none"; // hides quiz screen
-      document.getElementById("endScreen").style.display = "block"; // shows endScreen 
     }
-    setTimeout(renderNextQuestion, 1200);  
+    setTimeout(renderNextQuestion, 700);
   }
-};
+}
 
 function submitScore() {
-  document.getElementById("endScreen").style.display = "none"; 
+  document.getElementById("endScreen").style.display = "none";
   document.getElementById("scoreboardScreen").style.display = "block";
 
-  var initials = document.getElementById("initials").value;// get values "initials" from input box.
+  var initials = document.getElementById("initials").value; // get values "initials" from input box.
   var scores = JSON.parse(localStorage.getItem("scores")) || [];
-  
+
   scores.push({ score, initials });
 
   localStorage.setItem("scores", JSON.stringify(scores));
   // window.location.reload(); // refreshes the page
 
   getHighScores(scores);
-
-};
+}
 
 function getHighScores(hiScore) {
-  initials = hiScore[hiScore.length -1].initials;
-  score = hiScore[hiScore.length -1].score;
+  initials = hiScore[hiScore.length - 1].initials;
+  score = hiScore[hiScore.length - 1].score;
   console.log("Initials: " + initials + ", score: " + score); // gets the last object in the scores array from 5.
 
   document.getElementById("endScreen").style.display = "none";
@@ -134,20 +164,41 @@ function getHighScores(hiScore) {
   document.getElementById("highScore").style.display = "block";
 
   // maybe insert this in a for loop to show all the scores in the array.
-  document.getElementById("highScore").innerHTML = `Initials: ${initials}, Score: ${score}.`; // WHY DOESN'T THIS SHOW?bc display:none, somewhere
-  
+  document.getElementById(
+    "highScore"
+  ).innerHTML = `Initials: ${initials}, Score: ${score}.`; // WHY DOESN'T THIS SHOW?bc display:none, somewhere
+}
+
+function goBack() {
+  startQuiz();
+  window.location.reload();
 }
 
 function clearHighScores() {
   document.getElementById("highScore").style.display = "none";
 }
 
+/* TODO: 
 
-/*TODO:  
-- list item array for high scores.
-  sort array for higher score.
-- display final score and rankings list. could posibly highlight your current score.
-- missing border unden buttons when selction is chosen.
-- bug: 
-  + should wait 3 seconds after click to render next question. Currently, it cycle internally afte each btn click.
+bugs: 
++ when goBack btn is pressed it shows question and startQuiz sections.
++ last question does not display correct/wrong tag.
++ should wait 3 seconds after click to render next question. Currently, it cycle internally afte each btn click.
+
+- optional: list the array of previous score, a make high score appear at the top.
+- optional: make view highscore text clickable to display highscores table.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 */
